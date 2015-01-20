87ad2b8f7dc3010d48f34ca9abf3bacd4b65722c
sunbaoliang <blsun@redhat.com>
2015-01-20 11:07:57 +0800

    add cost price
        [sbl@baoliang post_price]$ python calc_price.py -w 2000 -m 2 -c 1500
        weight:2kg num:1 price:172
        weight:4kg num:2 price:139
        weight:6kg num:3 price:128
        weight:8kg num:4 price:122
        weight:10kg num:5 price:119
        weight:12kg num:6 price:116
        weight:14kg num:7 price:113
        weight:16kg num:8 price:112
        weight:18kg num:9 price:111
        weight:20kg num:10 price:109

--- post_price/calc_price.py
+++ post_price/calc_price.py
@@ -6,7 +6,7 @@ from price import *
 exchange_rate = 0.053
 def usage(app):
 	print "%s -w unit weight" %(app)
-	print "%s -p product price" %(app)
+	print "%s -c product_cost price" %(app)
 	print "%s -r exchange rate"  %(app)
 	print "%s -m transfer mode: 0:plane 1:sal 2:ship ,default is ship" %(app)
 	print "%s -h help" %(app)
@@ -33,8 +33,8 @@ if __name__ == "__main__":
 	weight = 2000
 	mode = 2
 	rate = exchange_rate
-	p_price = 0
-	options,args = getopt.getopt(sys.argv[1:],"hw:p:r:m:",["help","weight","rate","mode"])
+	cost_price = 0
+	options,args = getopt.getopt(sys.argv[1:],"hw:c:r:m:",["help","weight","rate","mode"])
 	#print options
 	for k,v in options:
 		if k in ("-h","--help"):
@@ -44,13 +44,13 @@ if __name__ == "__main__":
 			weight = int(v)
 		elif k in ("-r","--rate"):
 			exchange_rate = int(v)
-		elif k in ("-p"):
-			p_price = int(v)
+		elif k in ("-c"):
+			cost_price = int(v)
 		elif k in ("-m","--mode"):
 			mode = int(v)
 		else:
 			usage(sys.argv[0])
 			sys.exit(1)
 	for num in (1,2,3,4,5,6,7,8,9,10):
-		print "weight:%dkg num:%d price:%d" %(weight*num/1000, num, calc_price(weight,num,mode)*rate/num+p_price)
+		print "weight:%dkg num:%d price:%d" %(weight*num/1000, num, calc_price(weight,num,mode)*rate/num+cost_price*rate)
 
